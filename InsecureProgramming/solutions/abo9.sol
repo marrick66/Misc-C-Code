a98e2b91e9513d97d116335837d47211f973c84a
Sean Mayfield <marrick66@gmail.com>
2016-06-02 21:26:57 -0500

    More abo9 solution comments.

--- InsecureProgramming/solutions/abo9.sol
+++ InsecureProgramming/solutions/abo9.sol
@@ -8,6 +8,6 @@ I've dug into the most recent source of malloc, and I need to verify which metho
 
 Based on the size of the allocation, it looks like it's using the standard malloc best fit algorithm, retrieving from a bin at index 33 (256 + 4 + 4)/8 (we add 8 for the header size value and padding to make it divisible by 8). This verifies why the difference between the two chunks is 264.
 
-Since the gets() is unsafe, we can overflow the first chunk into the second, overwriting header and other data in the second. Doing this with garbage data will cause an error freeing the second chunk. 
+Since the gets() is unsafe, we can overflow the first chunk into the second, overwriting header and other data in the second. Doing this with garbage data will cause an error freeing the second chunk. From looking at the code of malloc, freeing a block who's previous block is in use only puts it back in it's bin.  If the previous block has been freed, it merges itself with the previous block and links it to the next block.  This can cause two writes, changing the previous block's forward pointer, and the next block's back pointer. This could allow arbitrary memory writing by manipulating the gets() overflow into the next block.  The problem is, the code checks the values of the forward and back pointers to make sure that they are valid.  This means that we really can't write arbitrarily.  I assume that when this challenge was written, this hadn't been introduced into the library yet or used a different malloc implementation.
 
 
